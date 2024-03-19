dc4c9d3e1120d8bc7c332f3d9f857adc537a1e17
unknown <josecastador1@gmail.com>
2024-03-19 09:01:42 -0400

    ki

--- kk.py
+++ kk.py
@@ -11,4 +11,4 @@ IDOLSDB_PATH = "/home/bing/dat2/idolsdb.db" # 2nd film_sources
 from icecream import ic
 IDOLSDB_PATH = "/home/bing/dat2/idolsdb.db"
 # Connect to an in-memory SQLite database
-conn = sqlite3.connect(':memory:')
+#conn = sqlite3.connect(':memory:')
