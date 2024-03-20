4a3165ee8f9f169ce5be6c206f747289d5f58832
unknown <josecastador1@gmail.com>
2024-03-20 14:37:15 -0400

    mahjort turn

--- test2222.py
+++ test2222.py
@@ -73,7 +73,7 @@ cur.execute ('''
                 join film_idols f3 on f1.film_name = f3.film_name
             where f1.idol_link like '%jav.guru%' and f2.idol_link like '%missav.com%' and f3.idol_link like '%javdatabase.com%'
             group by f1.idol_link, f2.idol_link, f3.idol_link
-             
+            having count(*) > 3
 
              
              ''')
@@ -82,7 +82,7 @@ for idol_1, idol_2, idol_3 in cur.fetchall():
     cur.execute('select idol_ID from idols where idol_link = ? or idol_link = ? or idol_link = ?', (idol_1, idol_2, idol_3))
     #print ('must be null', cur.fetchone())
     old_idol_id = cur.fetchone()
-    print (old_idol_id)
+    OO = -1
     if old_idol_id:
         oo = old_idol_id[0]
         cur.execute('insert or replace into idols values (?, ?)', (oo, idol_1))
@@ -90,14 +90,64 @@ for idol_1, idol_2, idol_3 in cur.fetchall():
         cur.execute('insert or replace into idols values (?, ?)', (oo, idol_3))
     else:
         new_idol_id = get_idol_id()
+        oo = new_idol_id
         cur.execute('insert into idols values (?, ?)', (new_idol_id, idol_1))
         cur.execute('insert into idols values (?, ?)', (new_idol_id, idol_2))
         cur.execute('insert into idols values (?, ?)', (new_idol_id, idol_3))
+    conn.commit()
+
+    # cur.execute ('''
+    #         select f1.film_name
+    #         from film_idols f1 
+    #             join film_idols f2 on f1.film_name = f2.film_name
+    #             join film_idols f3 on f1.film_name = f3.film_name
+    #         where f1.idol_link = ? and f2.idol_link = ? and  f3.idol_link = ?
+    #         group by f1.film_name
+    #          ''', (idol_1, idol_2, idol_3))
+    # r = cur.fetchall()
+
+    cur.execute ('''
+            select f1.film_name
+            from film_idols f1 
+                join film_idols f2 on f1.film_name = f2.film_name
+                join film_idols f3 on f1.film_name = f3.film_name
+            where (f1.idol_link = ? and f2.idol_link = ? and  f3.idol_link = ?) or
+            (f1.idol_link = ? and f2.idol_link = ?) or
+            (f1.idol_link = ? and f3.idol_link = ?) or
+            (f2.idol_link = ? and f3.idol_link = ?)
+            group by f1.film_name
+             ''', (idol_1, idol_2, idol_3, idol_1, idol_2, idol_1, idol_3, idol_2, idol_3))
+    r2 = cur.fetchall()
+    #print (idol_1, len(r), len(r2))
+    for film in r2:
+        ############################################
+        #TO DO: CREATE A TABLE THAT ASSOCIATES FILM WITH IDOL_ID
+        ############################################
+        cur.execute('delete from film_idols where film_name = ? and idol_link = ?', (film[0],idol_1))
+        cur.execute('delete from film_idols where film_name = ? and idol_link = ?', (film[0],idol_2))
+        cur.execute('delete from film_idols where film_name = ? and idol_link = ?', (film[0],idol_3))
+conn.commit()
+cur.execute ('select count(*) from film_idols')
+print ("after film_idols count:", cur.fetchone()[0])
+
+cur.execute ('select film_name, idol_link from film_idols where idol_link in (select idol_link from idols) order by film_name')
+s = cur.fetchall()
+print (len(s))
+for fn, il in s:
+    cur.execute('delete from film_idols where film_name = ? and idol_link = ?', (fn,il))
 conn.commit()
 
-cur.execute ('select * from idols')
-for row in cur.fetchall():
+cur.execute ('select count(*) from film_idols')
+print ("final film_idols count:", cur.fetchone()[0])
+fi = cur.execute ('select * from film_idols')
+
+for row in fi.fetchall():
     print (row)
+
+
+# cur.execute ('select * from idols order by idol_ID')
+# for row in cur.fetchall():
+#     print (row)
 
 
 # films_to_delete = cur.fetchall()
